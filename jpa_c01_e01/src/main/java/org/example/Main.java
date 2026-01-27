c6602e0309f933f763db198a14962e18a5b45b0c
Manish Bhatt <manishbhatt594@gmail.com>
2026-01-27 18:24:38 +0530

    Alternate way of creating EntityManagerFactory via programmatic approach
    Using a developer defined class implementing PersistenceUnitInfo
    interface that provides DB connection configuration properties, instead
    of using persistence.xml file for doing the same.

--- jpa_c01_e01/src/main/java/org/example/Main.java
+++ jpa_c01_e01/src/main/java/org/example/Main.java
@@ -1,18 +1,27 @@
 package org.example;
 
+import java.util.HashMap;
+
 import org.example.entities.Product;
+import org.example.persistence.CustomPersistenceUnitInfo;
+import org.hibernate.jpa.HibernatePersistenceProvider;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		// ------ USING persistence.xml -------------
 		// value of name attribute of <persistence-unit/> element in persistence.xml
 		// file
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+		// EntityManagerFactory emf =
+		// Persistence.createEntityManagerFactory("my-persistence-unit");
+
+		// ------ USING Programmatic Approach -------------
+		EntityManagerFactory emf = new HibernatePersistenceProvider()
+				.createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(), new HashMap<>());
 
 		EntityManager em = emf.createEntityManager(); // represents the context
 
@@ -20,8 +29,8 @@ public class Main {
 			em.getTransaction().begin();
 
 			Product product = new Product();
-			product.setId(1L);
-			product.setName("Reynolds Trimax Blue Rollerball Pen");
+			product.setId(2L);
+			product.setName("Butter Croissant");
 
 			// em.persist(product) --> add this to the context
 			// NOT AN INSERT QUERY
---
+++ jpa_c01_e01/src/main/java/org/example/persistence/CustomPersistenceUnitInfo.java
@@ -0,0 +1,121 @@
+package org.example.persistence;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import jakarta.persistence.SharedCacheMode;
+import jakarta.persistence.ValidationMode;
+import jakarta.persistence.spi.ClassTransformer;
+import jakarta.persistence.spi.PersistenceUnitInfo;
+import jakarta.persistence.spi.PersistenceUnitTransactionType;
+
+public class CustomPersistenceUnitInfo implements PersistenceUnitInfo {
+
+	@Override
+	public String getPersistenceUnitName() {
+		return "my-persistence-unit";
+	}
+
+	@Override
+	public String getPersistenceProviderClassName() {
+		return "org.hibernate.jpa.HibernatePersistenceProvider";
+	}
+
+	@Override
+	public PersistenceUnitTransactionType getTransactionType() {
+		return PersistenceUnitTransactionType.RESOURCE_LOCAL;
+	}
+
+	@Override
+	public DataSource getJtaDataSource() {
+		HikariDataSource dataSource = new HikariDataSource();
+
+		dataSource.setJdbcUrl("jdbc:mysql://localhost/laur_spilca_jpa_01");
+		dataSource.setUsername("root");
+		dataSource.setPassword("manish");
+
+		return dataSource;
+	}
+
+	@Override
+	public DataSource getNonJtaDataSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getMappingFileNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<URL> getJarFileUrls() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public URL getPersistenceUnitRootUrl() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getManagedClassNames() {
+		return List.of("org.example.entities.Product");
+	}
+
+	@Override
+	public boolean excludeUnlistedClasses() {
+		return false;
+	}
+
+	@Override
+	public SharedCacheMode getSharedCacheMode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ValidationMode getValidationMode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Properties getProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPersistenceXMLSchemaVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClassLoader getClassLoader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTransformer(ClassTransformer transformer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ClassLoader getNewTempClassLoader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
